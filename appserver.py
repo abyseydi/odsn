345d6433e6777eaa07f84e29e2a358d6b71102de
SND300228440 <aby@Accel-tech.net>
2025-07-21 16:54:33 +0000

    save

--- appserver.py
+++ appserver.py
@@ -12,7 +12,7 @@ DB_HOST = os.environ.get('DB_HOST', 'localhost')
 DB_PORT = os.environ.get('DB_PORT', '3306')
 DB_USER = os.environ.get('DB_USER', 'root')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
-DB_NAME = os.environ.get('DB_NAME', 'ansddb')
+DB_NAME = os.environ.get('DB_NAME', 'ansd')
 
 # Construction de l'URI de connexion
 DATABASE_URI = f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
