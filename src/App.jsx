3e70121f8a9b17882974972bfcf96e99850c5f1b
Shams230514 <84643634+Shams230514@users.noreply.github.com>
2025-08-25 16:55:28 +0000

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import GendarmerieAuth from "./layouts/gendarmerieAuth";
 import SafetyOrderHome from "./layouts/safetyOrderHome";
 import EducationHome from "./layouts/educationHome";
 // import Catalogue from "./layouts/Catalogue";
-// import PublicPolicyHome from "./layouts/PublicPolicyHome";
+import PublicPolicyHome from "./layouts/publicPolicyHome";
 
 
 
@@ -22,7 +22,7 @@ function App() {
       <Route path="/GendarmerieHome" element={<GendarmerieHome />} />
       <Route path="/EducationHome" element={<EducationHome />} />
       {/* <Route path="/Catalogue" element={<Catalogue />} /> */}
-      {/* <Route path="/PublicPolicyHome" element={<PublicPolicyHome />} /> */}
+      <Route path="/PublicPolicyHome" element={<PublicPolicyHome />} />
 
     </Routes>
   );
