08165f77fd635e057f77d2ef57c19d39baba3293
Poulo ❤️ <poulo@192.168.1.46>
2025-08-27 15:49:09 +0000

    start mobile

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,11 @@ import SafetyOrderHome from "./layouts/safetyOrderHome";
 import EducationHome from "./layouts/educationHome";
 import PublicPolicyHome from "./layouts/publicPolicyHome";
 import UseCasePage from "./layouts/UseCasePage";
-
 import SureteAuth from "./components/SureteAuth";
-
 import Catalogue from "./layouts/catalogue";
-
-
-
+import SanteHome from "./layouts/santeHome";
 import DashboardPage from "./layouts/ANSDHomeprime";
+import Perantal from "./layouts/perantal";
 
 function App() {
   return (
@@ -31,6 +28,8 @@ function App() {
       <Route path="/ANSDHomeprime" element={<DashboardPage />} />
 
       <Route path="/sureteAuth" element={<SureteAuth />} />
+      <Route path="/SanteHome" element={<SanteHome />} />
+      <Route path="/Perantal" element={<Perantal />} />
 
       <Route path="/UseCasePage" element={<UseCasePage />} />
     </Routes>
--- src/layouts/educationHome.jsx
+++ src/layouts/educationHome.jsx
@@ -1,18 +1,3 @@
-// import React from "react";
-
-// export default function EducationHome() {
-//   return (
-//     <div className="w-screen h-screen">
-//       <iframe
-//         title="Rapport Power BI"
-//         src="https://bi-srmt-srmt.apps.origins.heritage.africa/Reports/powerbi/MEN_Performance_Scolaire?rs:Embed=true"
-//         frameBorder="0"
-//         allowFullScreen={true}
-//         className="w-full h-full"
-//       ></iframe>
-//     </div>
-//   );
-// }
 
 
 import React, { useRef } from "react";
