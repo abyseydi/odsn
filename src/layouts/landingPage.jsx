37d6ef475d172615c9a838208d533dfe1b599f59
Shams230514 <84643634+Shams230514@users.noreply.github.com>
2025-09-03 12:45:48 +0000

    changement du 03/09

--- src/layouts/landingPage.jsx
+++ src/layouts/landingPage.jsx
@@ -1091,8 +1091,8 @@ const casUsage = {
     { title: "Éducation & Formation", img: "/img/education.png", link: "educationHome" },
     { title: "Politique publique", img: "/img/politique_publique.png", link: "/PublicPolicyHome" },
     { title: "Sûreté & ordre", img: "/img/fds_icon.png", link: "/SureteAuth" },
-    { title: "Économie, Finances & Budget", img: "/img/financement.png", link: "" },
-    { title: "Énergie & Mines", img: "/img/energie.png", link: "" },
+    //{ title: "Économie, Finances & Budget", img: "/img/financement.png", link: "" },
+    //{ title: "Énergie & Mines", img: "/img/energie.png", link: "" },
   ],
 };
 
